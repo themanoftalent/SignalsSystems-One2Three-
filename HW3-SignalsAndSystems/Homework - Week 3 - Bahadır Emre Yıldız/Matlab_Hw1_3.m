40eb145c5057ddd0118f4f515138d0473cd51d59
Emre <Emre@DESKTOP-R4I1EGE>
2024-03-09 23:17:17 +0300

    Week_3_Hw

---
+++ HW3-SignalsAndSystems/Homework - Week 3 - Bahadır Emre Yıldız/Matlab_Hw1_3.m
@@ -0,0 +1,9 @@
+num = input("Enter the Number you want to operate with: ");
+
+y = mod(num, 2);
+
+if( y == 1 )
+    disp("Your Number is odd.");
+elseif( y == 0 )
+    disp("Your Number is Even.");
+end
---
+++ HW3-SignalsAndSystems/Homework - Week 3 - Bahadır Emre Yıldız/Matlab_Hw1_5.m
@@ -0,0 +1,4 @@
+v = [1, 2, 3, 4, 5];
+
+summ = sum(v);
+disp(summ)
