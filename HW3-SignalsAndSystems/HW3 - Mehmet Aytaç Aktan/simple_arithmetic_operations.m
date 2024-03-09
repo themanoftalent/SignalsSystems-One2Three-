4c7391d514014f50eded06e1c869f9e660b29f66
Mehmet Aytaç Aktan <thebluevulpix@gmail.com>
2024-03-09 23:59:26 +0300

    Pushing homework files

---
+++ HW3-SignalsAndSystems/HW3 - Mehmet Aytaç Aktan/simple_arithmetic_operations.m
@@ -0,0 +1,13 @@
+% Simple Arithmetic Operations
+a = 10;
+b = 3;
+
+sum_result = a + b;
+diff_result = a - b;
+prod_result = a * b;
+quot_result = a / b;
+
+disp(['Sum: ', num2str(sum_result)]);
+disp(['Difference: ', num2str(diff_result)]);
+disp(['Product: ', num2str(prod_result)]);
+disp(['Quotient: ', num2str(quot_result)]);
---
+++ HW3-SignalsAndSystems/HW3 - Mehmet Aytaç Aktan/vector_operations.m
@@ -0,0 +1,7 @@
+% Vector Operations
+v = 1:5;
+sum_v = sum(v);
+
+disp(['Vector v: ', num2str(v)]);
+
+disp(['Sum of elements in v: ', num2str(sum_v)]);
