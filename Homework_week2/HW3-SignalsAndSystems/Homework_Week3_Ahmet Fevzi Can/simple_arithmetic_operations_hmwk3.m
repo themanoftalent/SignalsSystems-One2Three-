175217594a84ecf4e6982d52365339467c40bea0
Ahmet Fevzi Can <105818318+Ahmetfcan43@users.noreply.github.com>
2024-03-10 23:17:12 +0300

    Homework Week 3
    Ahmet Fevzi CAN 201504012

---
+++ Homework_week2/HW3-SignalsAndSystems/Homework_Week3_Ahmet Fevzi Can/simple_arithmetic_operations_hmwk3.m
@@ -0,0 +1,15 @@
+%Second question: Display the sum, difference and product of 2 numbers.
+num1 = 10;
+num2 = 3;
+
+% Sum
+sumresult = num1 + num2;
+disp(num2str(sumresult));
+
+% Difference
+diffresult = num1 - num2;
+disp(num2str(diffresult));
+
+% Product
+prodresult = num1 * num2;
+disp(num2str(prodresult));
