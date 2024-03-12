9ed792be60a7ec670e10d06c05195c531e9dc274
mertgoksu <mertgoksumg@gmail.com>
2024-03-12 15:26:16 +0300

    homework3-MertGöksu-201504024

---
+++ Homework 3 - MertGöksu - 201504024/homework3SimpleArithmeticOperations.m
@@ -0,0 +1,15 @@
+% Create a MATLAB script that calculates and displays the sum, 
+% difference, product, and quotient of two numbers 10 and 3.
+
+a = 10;
+b = 3;
+
+sum = a + b;
+difference = a - b;
+product = a * b;
+quotient = a / b;
+
+disp("Sum: " + sum);
+disp("Difference: " + difference);
+disp("Product: " + product);
+disp("Quotient: " + quotient);
